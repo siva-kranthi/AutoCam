e341225d21a70b3dc29675efd3d026a9cacca7c9
babu <558871+SivaKranthiKumar@users.noreply.github.com>
2020-04-20 18:12:32 +0530

    Added Splash Screen

--- src/main/index.js
+++ src/main/index.js
@@ -4,14 +4,51 @@ import { app, BrowserWindow } from "electron";
 import * as path from "path";
 import { format as formatUrl } from "url";
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
 let mainWindow;
 
 function createMainWindow() {
+  const splashWindow = new BrowserWindow({
+    width: 550,
+    height: 350,
+    backgroundColor: "#1e2124",
+    center: true,
+    frame: false,
+    resizable: false,
+    show: false,
+  });
+
+  splashWindow.loadFile(path.join(__static, "/splash.html"));
+
+  splashWindow.once("ready-to-show", () => {
+    splashWindow.show();
+  });
+
   const window = new BrowserWindow({
-    webPreferences: { nodeIntegration: true },
+    width: 1280,
+    height: 720,
+    minWidth: 830,
+    minHeight: 430,
+    backgroundColor: "#2a2522",
+    show: false,
+    titleBarStyle: "hidden",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    icon: path.join(__static, "/logo.png"),
+    title: "AutoCam",
+  });
+
+  // if main window is ready to show, then destroy the splash window and show up the main window
+  window.once("ready-to-show", () => {
+    splashWindow.destroy();
+    window.show();
   });
 
   if (isDevelopment) {
@@ -44,18 +81,20 @@ function createMainWindow() {
   return window;
 }
 
-// quit application when all windows are closed
-app.on("window-all-closed", () => {
-  // on macOS it is common for applications to stay open until the user explicitly quits
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on("activate", () => {
-  // on macOS it is common to re-create a window even after all windows have been closed
-  if (mainWindow === null) {
-    mainWindow = createMainWindow();
+app.on("activate", function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
   }
 });
 
@@ -63,3 +102,15 @@ app.on("activate", () => {
 app.on("ready", () => {
   mainWindow = createMainWindow();
 });
+
+function splashWindow() {
+  const splashWindow = new BrowserWindow({
+    width: 550,
+    height: 350,
+    backgroundColor: "#1e2124",
+    center: true,
+    frame: false,
+    resizable: false,
+    show: false,
+  });
+}
